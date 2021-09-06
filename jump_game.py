7513c9bf15b9d41b4d49c4b2ef42023ba2be5acf
p.arya19 <pratibhaaryajan19@gmail.com>
2021-09-06 23:08:31 +0530

    comment

--- jump_game.py
+++ jump_game.py
@@ -4,6 +4,7 @@ You are given an integer array nums. You are initially positioned at the array's
 Return true if you can reach the last index, or false otherwise.
 """
 """leetcode question"""
+"""hello"""
 class Solution:
     def canJump(self, n: List[int]) -> bool:
     	m = 0
